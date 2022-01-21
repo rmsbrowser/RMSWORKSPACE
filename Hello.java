5af1c6aaaa40d414e923be851e369ca1a6d28de2
RmsBrowser <subscribe@ramsescaldas.com>
2022-01-21 01:21:17 +0000

    update02

--- Hello.java
+++ Hello.java
@@ -6,7 +6,7 @@
 // Todo programa tem um ponto de entrada: o "método" main de alguma "classe"
 public class Hello {
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, world!, from Ramsés Caldas");
   }
 }
 
