a7cc467cd76b23cb3298f48b2c1883fb6493adf3
rmsbrowser <subscribe@ramsescaldas.com>
2022-01-20 21:58:40 -0300

    Update-MainFolder

---
+++ Hello.java
@@ -0,0 +1,13 @@
+/*
+ * O primeiro programa em Java: Hello World
+ * Autor: Ramsés Augusto de Lima Caldas
+ */
+
+// Todo programa tem um ponto de entrada: o "método" main de alguma "classe"
+public class Hello {
+  public static void main(String[] args) {
+    System.out.println("Hello, world!");
+  }
+}
+
+
